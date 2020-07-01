29ea8713a8384fef8c717df24132da0fa8744be8
Ahmed Alani <s3leha@gmail.com>
2020-07-01 07:14:23 -0700

    lint fix

--- 4_queries/12_name_of_teacher_that_assisted.sql
+++ 4_queries/12_name_of_teacher_that_assisted.sql
@@ -1,4 +1,4 @@
-SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort, 
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
   FROM teachers
   JOIN assistance_requests ON teacher_id = teachers.id
   JOIN students ON student_id = students.id
